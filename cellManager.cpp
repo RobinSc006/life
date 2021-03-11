79e3861200b27d2650d39bb9a0290f8080d353bc
robin schulz <robinschulz3000@gmail.com>
2021-03-11 07:29:40 +0100

    Add status display(text), improve cell manipulation, improve ui

--- cellManager.cpp
+++ cellManager.cpp
@@ -7,22 +7,37 @@ private:
     const int MIN_CONNECTED = 2;
     const int MAX_CONNECTED = 3;
 
+    int generations = 0;
+
+    float map(float n, float start1, float stop1, float start2, float stop2)
+    {
+        return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+    }
+
 public:
-    static const int GRID_WIDTH = 152;
-    static const int GRID_HEIGHT = 152;
+    static const int GRID_WIDTH = 151;
+    static const int GRID_HEIGHT = 151;
     Cell cells[GRID_WIDTH][GRID_HEIGHT];
 
     const int GRID_SEPERATOR_SIZE = 1;
-    const int GRID_X_OFFSET = 15;
-    const int GRID_Y_OFFSET = 15;
+    const int GRID_X_OFFSET = 13;
+    const int GRID_Y_OFFSET = 20;
 
-    void set_all_alive(bool status)
+    int get_generations()
     {
+        return generations;
+    }
+
+    void reset_cells(bool status)
+    {
+        generations = 0;
         for (int x = 0; x < GRID_WIDTH; x++)
         {
             for (int y = 0; y < GRID_HEIGHT; y++)
             {
                 cells[x][y].set_alive(status);
+                cells[x][y].set_die_next_turn(false);
+                cells[x][y].set_live_next_turn(false);
                 // ? It didn't work before, because I wrote cells[x, y]...
             }
         }
@@ -40,17 +55,31 @@ public:
         }
     }
 
-    void update_cells()
+    void get_user_set_cells(sf::RenderWindow &window, bool status)
     {
+        sf::Vector2i mouse_pos = sf::Mouse::getPosition(window);
+        int set_cell_x = map(mouse_pos.x, 0, window.getSize().x - (GRID_X_OFFSET - GRID_SEPERATOR_SIZE), 0, GRID_WIDTH);
+        int set_cell_y = map(mouse_pos.y, 0, window.getSize().y - (GRID_Y_OFFSET - GRID_SEPERATOR_SIZE), 0, GRID_HEIGHT);
+        if (set_cell_x < GRID_WIDTH && set_cell_y < GRID_HEIGHT && set_cell_x >= 0 && set_cell_y >= 0)
+            cells[set_cell_x][set_cell_y].set_alive(status);
+    }
+
+    bool update_cells()
+    {
+        int living_cells = 0;
+
+        generations += 1;
         for (int x = 0; x < GRID_WIDTH; x++)
         {
             for (int y = 0; y < GRID_HEIGHT; y++)
             {
-                if (cells[x][y].get_die_next_turn()) {
+                if (cells[x][y].get_die_next_turn())
+                {
                     cells[x][y].set_alive(false);
                     cells[x][y].set_die_next_turn(false);
                 }
-                if (cells[x][y].get_live_next_turn()) {
+                if (cells[x][y].get_live_next_turn())
+                {
                     cells[x][y].set_alive(true);
                     cells[x][y].set_live_next_turn(false);
                 }
@@ -61,7 +90,6 @@ public:
         {
             for (int y = 0; y < GRID_HEIGHT; y++)
             {
-
                 bool left_connected = false;
                 bool right_connected = false;
                 bool up_connected = false;
@@ -71,6 +99,9 @@ public:
                 bool diagonal_down_right_connected = false;
                 bool diagonal_up_left_connected = false;
                 bool diagonal_up_right_connected = false;
+
+                if (cells[x][y].is_alive())
+                    living_cells += 1;
 
                 /* Horizontal/Vertical */
                 if (x > 0 && cells[x - 1][y].is_alive())
@@ -127,5 +158,9 @@ public:
                 }
             }
         }
+        if (living_cells == 0)
+            return false;
+        else
+            return true;
     }
 };
--- main.cpp
+++ main.cpp
@@ -7,28 +7,57 @@
 
 int main()
 {
-    const int WIDTH = 790;
-    const int HEIGHT = 790;
+    const int WIDTH = 780;
+    const int HEIGHT = 780;
 
     const int UPDATE_INTERVAL_INCREMENT = 1;
 
-    int keypress_cooldown = 30;
+    int keypress_cooldown = 10;
     int keypress_counter = 0;
 
     int update_counter = 0;
     int update_interval = 5;
 
+    sf::Font font;
+    if (!font.loadFromFile("assets/font/Recursive-Light.ttf"))
+        std::cout << "unable to load font\n";
+
+    sf::Text update_interval_display;
+    sf::Text paused_display;
+    sf::Text generations_display;
+
+    update_interval_display.setFont(font);
+    paused_display.setFont(font);
+    generations_display.setFont(font);
+
+    update_interval_display.setCharacterSize(11);
+    paused_display.setCharacterSize(11);
+    generations_display.setCharacterSize(11);
+
+    update_interval_display.setPosition(10.0, 0.0);
+    paused_display.setPosition(235.0, 0.0);
+    generations_display.setPosition(125.0, 0.0);
+
+    update_interval_display.setFillColor(sf::Color::White);
+    paused_display.setFillColor(sf::Color::Red);
+    generations_display.setFillColor(sf::Color::White);
+
     // ? Game parameters
-    sf::Color COLOR_WHITE = sf::Color(233, 233, 233, 255);
-    sf::Color COLOR_RED = sf::Color(255, 54, 54, 255);
-    sf::Color COLOR_GREEN_ONE = sf::Color(0, 245, 92, 255);
-    sf::Color COLOR_GREEN_TWO = sf::Color(135, 168, 144, 255);
-    sf::Color COLOR_BLUE = sf::Color(41, 38, 224, 233);
+    const sf::Color COLOR_WHITE = sf::Color(233, 233, 233, 255);
+    const sf::Color COLOR_RED = sf::Color(255, 54, 54, 255);
+    const sf::Color COLOR_GREEN_ONE = sf::Color(0, 245, 92, 255);
+    const sf::Color COLOR_GREEN_TWO = sf::Color(135, 168, 144, 255);
+    const sf::Color COLOR_BLUE = sf::Color(41, 38, 224, 233);
+    const sf::Color CLEAR_COLOR = sf::Color(15, 15, 15, 233);
+
+    const sf::Color SELECT_COLOR = sf::Color(242, 7, 152, 255);
 
     sf::Color current_color = COLOR_GREEN_TWO;
 
+    bool update_paused = true;
+
     CellManager cell_manager;
-    cell_manager.set_all_alive(false);
+    cell_manager.reset_cells(false);
 
     // ? R-pentomino
     cell_manager.cells[25 + 60][23 + 60].set_alive(true);
@@ -40,16 +69,24 @@ int main()
 
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Game of Life");
 
+    // ? Main loop
     while (window.isOpen())
     {
+        update_interval_display.setString("Update interval: " + std::to_string(update_interval));
+        generations_display.setString("Generations: " + std::to_string(cell_manager.get_generations()));
+        if (update_paused)
+            paused_display.setString("Paused");
+        else
+            paused_display.setString("");
+
         if (keypress_counter - 10 >= INT32_MAX)
             keypress_counter = 0;
         keypress_counter++;
         update_counter++;
 
-        if (update_counter >= update_interval)
+        if (!update_paused && update_counter >= update_interval)
         {
-            cell_manager.update_cells();
+            if(!cell_manager.update_cells()) update_paused = true;
             update_counter = 0;
         }
 
@@ -58,6 +95,7 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && keypress_counter >= keypress_cooldown)
             {
                 update_interval -= UPDATE_INTERVAL_INCREMENT;
@@ -94,11 +132,38 @@ int main()
                 current_color = COLOR_BLUE;
                 keypress_counter = 0;
             }
+
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+            {
+                cell_manager.get_user_set_cells(window, true);
+            }
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+            {
+                cell_manager.get_user_set_cells(window, false);
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && keypress_counter >= keypress_cooldown)
+            {
+                if (!update_paused)
+                    update_paused = true;
+                else
+                    update_paused = false;
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && keypress_counter >= keypress_cooldown)
+            {
+                cell_manager.reset_cells(false);
+            }
         }
 
-        window.clear();
+        window.clear(CLEAR_COLOR);
 
         cell_manager.render_cells(window, current_color);
+
+        window.draw(update_interval_display);
+        window.draw(paused_display);
+        window.draw(generations_display);
+
         window.display();
     }
 
